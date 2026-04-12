1e6231ea4246de142de8163e29482a354488f756
AhmedHesham <ahmed.hesham303303@gmail.com>
2026-04-12 15:03:42 +0200

    chore : added styled components

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,27 @@
 import styled from "styled-components";
 import GlobalStyles from "./styles/GlobalStyles";
+import Button from "./ui/Button";
+import Input from "./ui/Input";
 const H1 = styled.h1`
   font-size: 30px;
   font-weight: 600;
-  color: blue;
+  color: yellow;
+`;
+
+const StyledApp = styled.main`
+  background-color: orangered;
+  padding: 20px;
 `;
 const App = () => {
   return (
     <>
       <GlobalStyles />
-      <H1>Welcome to React</H1>
+      <StyledApp>
+        <H1>Welcome to React</H1>
+        <Button>click</Button>
+        <Button>click</Button>
+        <Input placeholder="Enter your name" />
+      </StyledApp>
     </>
   );
 };
