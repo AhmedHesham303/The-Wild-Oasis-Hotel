6c7cf9889c5208c7cb5e231ef244c0cedc9c41bc
AhmedHesham <ahmed.hesham303303@gmail.com>
2026-04-12 14:41:37 +0200

    chore : project setup

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+import GlobalStyles from "./styles/GlobalStyles";
+const H1 = styled.h1`
+  font-size: 30px;
+  font-weight: 600;
+  color: blue;
+`;
+const App = () => {
+  return (
+    <>
+      <GlobalStyles />
+      <H1>Welcome to React</H1>
+    </>
+  );
+};
+
+export default App;
